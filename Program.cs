dd53849bc9330f3edd50589db8a4484660110e61
recovery7 <recovery7boot@gmail.com>
2021-03-23 01:17:10 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NumberSystem;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начальное число:   ");
+            string mainString = Console.ReadLine();
+
+            Console.WriteLine("Введите начальную систему счисления:   ");
+            int firstSystem = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Введите конечную систему счисления:   ");
+            try
+            {
+                int secondSystem = Convert.ToInt32(Console.ReadLine());
+                char[] result = translateNumbers.translateMain(mainString, firstSystem, secondSystem);
+                Console.WriteLine(result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Некорректный ввод.");
+            }      
+        }
+    }
+}
